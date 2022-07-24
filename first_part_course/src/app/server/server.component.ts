db8b6eaf612632225c9c1a18511dc12497b684b4
william <william.steinkedemello@gmail.com>
2022-07-24 13:16:06 +0200

    add string interpolation in order to stablish a communication between template and business logic in TS

--- first_part_course/src/app/server/server.component.ts
+++ first_part_course/src/app/server/server.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
+  serverId: string = '151.777.231.12';
+  serverStatus: string = 'running';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getServerStatus() {
+    return this.serverStatus;
   }
-
 }
