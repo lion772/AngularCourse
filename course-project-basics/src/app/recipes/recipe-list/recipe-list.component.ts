3cd62a95dbc9b891eb1b9defcb461f4ad87fe3af
william <william.steinkedemello@gmail.com>
2022-07-27 22:31:57 +0200

    do some alterations

--- course-project-basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Array<string> = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Honey Mustard Chicken',
+      'Asian Food',
+      'https://s23209.pcdn.co/wp-content/uploads/2019/03/Honey-Mustard-Chicken-SaladIMG_8674-500x750.jpg'
+    ),
+    new Recipe(
+      'Sheet Pan Shrimp Fajitas',
+      'Asian Food',
+      'https://s23209.pcdn.co/wp-content/uploads/2022/02/211015_DAMN-DELICIOUS_Shrimp-Fajitas_051-480x720.jpg'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  onCreateNewRecipe() {
+    //this.recipes.push(new Recipe());
+  }
 }
