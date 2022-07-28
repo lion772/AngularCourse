d6e486901d54b6d563470132209dc1095778395f
william <william.steinkedemello@gmail.com>
2022-07-28 13:11:26 +0200

    bind custom property through Alias

--- cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: { name: string; type: string; content: string };
+  //@Input() element: { name: string; type: string; content: string };
+  @Input("srvElement") element: { name: string; type: string; content: string };
 
   constructor() {}
 
