52dc107ce57f6851e706274f125bacb744305be7
william <william.steinkedemello@gmail.com>
2022-07-28 12:46:03 +0200

    bind to custom properties

--- cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { name: "server", type: "server", content: "just a server!" },
+    { name: "blueprint", type: "blueprint", content: "just a server!" },
+  ];
 }
--- cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-cockpit",
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class CockpitComponent implements OnInit {
   newServerName = "";
   newServerContent = "";
+  @Input() serverElements = [];
 
   constructor() {}
 
