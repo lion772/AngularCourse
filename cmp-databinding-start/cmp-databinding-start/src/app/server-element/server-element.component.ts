0359e9e0c195f2d942ff6ebb44cd16d6862ef84c
william <william.steinkedemello@gmail.com>
2022-07-30 12:29:24 +0200

    project content into components via ng-content

--- cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ContentChild,
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterViewInit {
   //@Input() element: { name: string; type: string; content: string };
   @Input("srvElement") element: { name: string; type: string; content: string };
+  @ViewChild("heading", { static: true }) header: ElementRef;
+  @ContentChild("paragraphContent", { static: true })
+  paragraph: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    "I could also use ngAfterContentInit method";
+    //console.log("Header value: ", this.header.nativeElement.textContent);
+    console.log(
+      "Paragraph Content: ",
+      this.paragraph.nativeElement.textContent
+    );
+  }
 }
