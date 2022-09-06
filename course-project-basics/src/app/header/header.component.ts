24e9d0bcbe0b6d880f835e2d23c18cc1c66c9265
william <william.steinkedemello@gmail.com>
2022-09-06 14:53:27 +0200

    fix page reload issues

--- course-project-basics/src/app/header/header.component.ts
+++ course-project-basics/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,26 +9,7 @@ export class HeaderComponent implements OnInit {
   collapsed: boolean = true;
   toggle: boolean = false;
 
-  @Output() displayRecipeOrShoppingListener = new EventEmitter<{
-    isShoppingList: boolean;
-    isRecipes: boolean;
-  }>();
-
   constructor() {}
 
-  ngOnInit(): void {}
-
-  onRecipeClicked() {
-    this.displayRecipeOrShoppingListener.emit({
-      isRecipes: true,
-      isShoppingList: false,
-    });
-  }
-
-  onShoppingClicked() {
-    this.displayRecipeOrShoppingListener.emit({
-      isRecipes: false,
-      isShoppingList: true,
-    });
-  }
+  ngOnInit() {}
 }
