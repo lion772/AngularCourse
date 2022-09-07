fb902a1efe29489f7aa258b9190c9016273a081e
william <william.steinkedemello@gmail.com>
2022-09-07 09:49:36 +0200

    Configure route parameters and enable recently deactivated links

--- course-project-basics/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project-basics/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +10,21 @@ import { Ingredient } from '../../shared/ingredient.model';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() inputRecipeSelected: Recipe;
+  inputRecipeSelected: Recipe;
+  id: number;
   toggle: boolean = false;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((param: Params) => {
+      this.id = +param['id'];
+      this.inputRecipeSelected = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(
--- course-project-basics/src/app/recipes/recipe.service.ts
+++ course-project-basics/src/app/recipes/recipe.service.ts
@@ -34,6 +34,10 @@ export class RecipeService {
     return [...this.recipes];
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]): void {
     this.shoppingListService.addIngredients(ingredients);
   }
