8fad2afeb7cb54b99032fe2706e2fed6396892c2
william <william.steinkedemello@gmail.com>
2022-09-07 10:39:47 +0200

    add Edit routes

--- course-project-basics/src/app/app.module.ts
+++ course-project-basics/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { UnlessDirective } from './directives/unless.directive';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     UnlessDirective,
     DropdownDirective,
     RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
